0015b5ceaa0888aca5fd69740197525db5009a28
Mohammed SAID <4593651+TheNeptuneZ@users.noreply.github.com>
2018-03-27 10:08:54 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAUWznUkY6pMVhmBNdynbG5mrfZvWWL_Pc",
-    authDomain: "learning-app-angular.firebaseapp.com",
-    databaseURL: "https://learning-app-angular.firebaseio.com",
-    projectId: "learning-app-angular",
-    storageBucket: "learning-app-angular.appspot.com",
-    messagingSenderId: "254995473413"
+    // Code to update if you want to link the project to your firebase db 
+        apiKey: "AIzaSyAUWznUkY6pMVhmBNdynbG5mrfZvWWL_Pc",
+        authDomain: "learning-app-angular.firebaseapp.com",
+        databaseURL: "https://learning-app-angular.firebaseio.com",
+        projectId: "learning-app-angular",
+        storageBucket: "learning-app-angular.appspot.com",
+        messagingSenderId: "254995473413"
   }
 };
